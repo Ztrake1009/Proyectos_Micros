0d9dfaf6351bd1873cf10cec818606edd1d534c5
Ztrake1009 <gusvarso1019@gmail.com>
2023-09-22 18:03:02 -0600

    Update Interfaz.py

--- Proyecto Microprocesador/Interfaz.py
+++ Proyecto Microprocesador/Interfaz.py
@@ -22,6 +22,7 @@ class Interfaz:
         self.boton_Volver = None
         self.boton_Iniciar = None
 
+    # Selecciona el archivo de excel para saber que cajas deben ir en la matriz de carga.
     def abrir_Excel(self):
         archivo_path = filedialog.askopenfilename(filetypes=[("Archivos Excel", "*.xlsx")])
         if archivo_path:
@@ -43,13 +44,16 @@ class Interfaz:
         self.boton_Modo1.destroy()
         self.boton_Modo2.destroy()
 
+        self.Mensaje = tk.Label(ventana, text="En espera", font=("Arial", 16))
+        self.Mensaje.pack(side="bottom")
+
         self.boton_Volver = tk.Button(ventana, text="Volver a pantalla principal", command=self.volver_Inicio)
         self.boton_Volver.pack(side="top")
 
         self.i = 0
         self.j = 0
 
-        self.boton_Iniciar = tk.Button(ventana, text="Iniciar Acomodo", command=self.detectar_Cajas)
+        self.boton_Iniciar = tk.Button(ventana, text="Iniciar Acomodo", command=self.acomodar_Cajas)
         self.boton_Iniciar.pack(side="top")
 
         self.tamX_Matriz1 = 8  # Cantidad columnas de la zona de suministro 1.
@@ -61,6 +65,7 @@ class Interfaz:
         self.tam_Matriz3 = 5  # Tamaño de la zona de carga.
 
         self.matriz1 = [[None for _ in range(self.tamX_Matriz1)] for _ in range(self.tamY_Matriz1)]  # Cantidad en X, Cantidad en Y.
+        self.matriz1.append([None]) #Crea un nuevo espacio en la matriz para que sean 25
         self.matriz2 = [[None for _ in range(self.tamX_Matriz2)] for _ in range(self.tamY_Matriz2)]
         self.matriz3 = [[None for _ in range(self.tam_Matriz3)] for _ in range(self.tam_Matriz3)]
 
@@ -96,9 +101,6 @@ class Interfaz:
         self.crear_Matriz_1()
         self.crear_Matriz_2()
         self.crear_Matriz_3()
-
-        #print(self.espacios_Matriz1)
-        #[116,116],[156,116]
         
         # Crea el objeto representativo de la grúa.
         self.movimiento_X = 1
@@ -110,7 +112,7 @@ class Interfaz:
         self.Y_Final = self.Y_Actual + self.tam_Celdas_Carga
         self.grua = self.lienzo.create_oval(self.X_Actual, self.Y_Actual, self.Y_Final, self.Y_Final, width=1, fill="orange")
 
-    # Carga el archivo de excel de prueba para la interfaz y modifica los colores.
+    # Carga el archivo de excel de prueba para la matriz de suministro.
     def leer_Excel_Prueba(self):
         archivo_path = filedialog.askopenfilename(filetypes=[("Archivos Excel", "*.xlsx")])
         if archivo_path:
@@ -119,21 +121,15 @@ class Interfaz:
             self.letras_Suministro1 = [cell.value for row in hoja.iter_rows() for cell in row]
             libro.close()
 
-        self.letras_Suministro2.append(self.letras_Suministro1[24])
-        self.letras_Suministro1.pop()
-        i = 1
-        while (i < 15):
+        i = 0
+        while (i < 16):
             self.letras_Suministro2.append("")
             i += 1
-
-        #print(self.letras_Suministro1)
-        #print(self.letras_Suministro2)
 
     # Crea la Matriz 1.
     def crear_Matriz_1(self):
         self.leer_Excel_Prueba()
         letra_actual = 0
-
         for fila_Matriz1 in range(self.tamY_Matriz1):
             for col_Matriz1 in range(self.tamX_Matriz1):
                 x0 = self.tam_Bases + self.tam_Esp_Libre + (col_Matriz1 * self.tam_Celdas_Suministro)
@@ -145,12 +141,27 @@ class Interfaz:
                 self.matriz1[fila_Matriz1][col_Matriz1] = self.lienzo.create_rectangle(x0, y0, x1, y1, fill=color)
                 self.espacios_Matriz1.append([x0,y0])
 
+                # Crea el ultimo espacio de la matriz de manera que se "sale" de los 8x3 espacios originales, esto para que sean 25 espacios en total
+                if (fila_Matriz1 == 2) and (col_Matriz1 == 7):
+                    col_Matriz1 = 0
+                    fila_Matriz1 = 3
+                    x0 = self.tam_Bases + self.tam_Esp_Libre + (col_Matriz1 * self.tam_Celdas_Suministro)
+                    y0 = self.tam_Bases + self.tam_Esp_Libre + (fila_Matriz1 * self.tam_Celdas_Suministro)
+                    x1 = x0 + self.tam_Celdas_Suministro
+                    y1 = y0 + self.tam_Celdas_Suministro                    
+                    color = self.ident_Color(letra_actual, self.letras_Suministro1)
+                    self.matriz1[fila_Matriz1][col_Matriz1] = self.lienzo.create_rectangle(x0, y0, x1, y1, fill=color)
+                    self.espacios_Matriz1.append([x0,y0])
+                
     # Crea la Matriz 2.
     def crear_Matriz_2(self):
         letra_actual = 0
-
         for fila_Matriz2 in range(self.tamY_Matriz2):
             for col_Matriz2 in range(self.tamX_Matriz2):
+                # Se salta de crear el primer espacio, esto para que sea el ultimo espacio de la Matriz 1.
+                if (fila_Matriz2 == 0) and (col_Matriz2 == 0):
+                    col_Matriz2 = 1
+                
                 x0 = self.tam_Bases + self.tam_Esp_Libre + (col_Matriz2 * self.tam_Celdas_Suministro)
                 y0 = self.tam_Bases + self.tam_Esp_Libre + (self.tam_Celdas_Suministro * self.tamY_Matriz1) + (fila_Matriz2 * self.tam_Celdas_Suministro)
                 x1 = x0 + self.tam_Celdas_Suministro
@@ -174,10 +185,12 @@ class Interfaz:
                 self.matriz3[fila_Matriz3][col_Matriz3] = self.lienzo.create_rectangle(x0, y0, x1, y1, width=3, fill=color)
                 self.espacios_Matriz3.append([x0,y0])
 
+    # Vuelve al menu principal.
     def volver_Inicio(self):
         self.lienzo.destroy()
         self.boton_Volver.destroy()
         self.boton_Iniciar.destroy()
+        self.Mensaje.destroy()
 
         self.letras_Suministro1 = []
         self.letras_Suministro2 = []
@@ -186,6 +199,7 @@ class Interfaz:
         self.boton_Cargar = tk.Button(ventana, text="Cargar Archivo Excel", command=self.abrir_Excel)
         self.boton_Cargar.pack(side="top")
 
+    # Define el color del espacio de la matriz dependiendo de la lista que reciba.
     def ident_Color(self, letra_actual, letras):
         while letra_actual < len(letras):
             if letras[letra_actual] == "R":  # Rojo Claro.
@@ -200,96 +214,8 @@ class Interfaz:
             else:
                 return "white"
             
-    def detectar_Cajas(self):
-        
-        #listo = 0
-        """
-        self.X_Destino = self.espacios_Matriz1[3][0]
-        self.Y_Destino = self.espacios_Matriz1[3][1]
-
-        # Ejecuta el movimiento.
-        self.mover_Grua()
-        time.sleep(1)
-        ventana.update()
-
-        self.actualizar_Suministro1()
-        time.sleep(2)
-        ventana.update()
-
-        self.X_Destino = self.espacios_Matriz3[0][0]
-        self.Y_Destino = self.espacios_Matriz3[0][1]
-
-        self.mover_Grua()
-        time.sleep(1)
-        ventana.update()
-
-        #self.ventana.after(2000, self.actualizar_Carga(self.letras_Carga[0]))
-        self.actualizar_Carga(self.letras_Carga[0])
-        time.sleep(2)
-        ventana.update()
-        """
-
-        """
-        if (self.letras_Suministro1[self.i] == self.letras_Carga[self.j]):
-
-            self.X_Destino = self.espacios_Matriz1[self.i][0]
-            self.Y_Destino = self.espacios_Matriz1[self.i][1]
-
-            # Ejecuta el movimiento.
-            self.ventana.after(100, self.mover_Grua)
-            #self.mover_Grua()
-
-            #time.sleep(2)
-
-            self.ventana.after(100, self.actualizar_Suministro1)
-            #time.sleep(2)
-
-            self.X_Destino = self.espacios_Matriz3[self.j][0]
-            self.Y_Destino = self.espacios_Matriz3[self.j][1]
-
-            self.ventana.after(100, self.mover_Grua)
-            #time.sleep(2)
-
-            self.ventana.after(100, self.actualizar_Carga(self.letras_Carga[self.j]))
-            #time.sleep(2)
-        
-        if self.j == len(self.letras_Carga)-2:
-            self.j = 0
-            self.i += 1
-        
-        if self.i == len(self.letras_Suministro1):
-            return
-        
-        self.j += 1
-        self.detectar_Cajas()
-        """
-
-        """
-        self.X_Destino = self.espacios_Matriz1[2][0]
-        self.Y_Destino = self.espacios_Matriz1[2][1]
-        listo = self.mover_Grua_X()
-        #self.mover_Grua_Y()
-        
-        if (listo == 1):
-
-            listo = 0
-
-            self.actualizar_Suministro1()
-            
-            self.X_Destino = self.espacios_Matriz3[1][0]
-            self.Y_Destino = self.espacios_Matriz3[1][1]
-            #self.ventana.after(1000, self.mover_Grua_X)
-            listo = self.mover_Grua_X()
-
-            #self.ventana.after_cancel(self.detectar_Cajas)
-            #self.mover_Grua_Y()
-
-            #if (listo == 1):
-            #    self.actualizar_Carga(self.letras_Carga[1])
-
-        #self.ventana.after(20, self.detectar_Cajas)
-
-        """
+    def acomodar_Cajas(self):
+        self.boton_Iniciar.destroy()
 
         for pos_Sum1, elem_Sum1 in enumerate(self.letras_Suministro1):
             for pos_Carga, elem_Carga in enumerate(self.letras_Carga):
@@ -297,29 +223,53 @@ class Interfaz:
                     self.X_Destino = self.espacios_Matriz1[pos_Sum1][0]
                     self.Y_Destino = self.espacios_Matriz1[pos_Sum1][1]
 
-                    # Ejecuta el movimiento.
-                    self.mover_Grua()
-                    time.sleep(1)
+                    # Ejecuta el movimiento, primero en X y luego en Y.
+                    # Primero se mueve hacia la caja que debe recoger.
+                    self.Mensaje.config(text="Moviendo Grua hacia la caja por recoger.")
+                    self.mover_Grua_X()
+                    time.sleep(0.1)
+                    ventana.update()
+
+                    self.mover_Grua_Y()
+                    time.sleep(0.1)
+                    self.Mensaje.config(text="Recogiendo caja.")
                     ventana.update()
 
                     self.actualizar_Suministro1()
-                    time.sleep(2)
+                    time.sleep(0.1)
                     ventana.update()
 
                     self.X_Destino = self.espacios_Matriz3[pos_Carga][0]
                     self.Y_Destino = self.espacios_Matriz3[pos_Carga][1]
-
-                    self.mover_Grua()
-                    time.sleep(1)
+                    
+                    # Ahora se mueve hacia el espacio donde debe dejar la caja.
+                    self.Mensaje.config(text="Moviendo Grua hacia el espacio designado.")
+                    self.mover_Grua_X()
+                    time.sleep(0.1)
                     ventana.update()
 
-                    #self.ventana.after(2000, self.actualizar_Carga(self.letras_Carga[0]))
+                    self.mover_Grua_Y()
+                    time.sleep(0.1)
+                    self.Mensaje.config(text="Dejando caja.")
+                    ventana.update()
+
                     self.actualizar_Carga(self.letras_Carga[pos_Carga])
-                    time.sleep(2)
+                    time.sleep(0.1)
                     ventana.update()
                     self.letras_Carga[pos_Carga] = 0
                     break
 
+        # Se mueve a la posición inicial.
+        self.Mensaje.config(text="Volviendo a la posición inicial.")
+        self.X_Destino = self.tam_Bases
+        self.Y_Destino = self.tam_Bases
+        self.mover_Grua_X()
+        time.sleep(2)
+        ventana.update()
+
+        self.mover_Grua_Y()
+        time.sleep(2)
+        ventana.update()
 
     def actualizar_Suministro1(self):
         # Actualiza la matriz de la zona de Suministro 1.
@@ -339,37 +289,39 @@ class Interfaz:
         elif letra == "B":  # Azul Claro.
             color = "#0000FF"
 
-        # Actualiza la matriz de la zona de Suministro 1.
+        # Actualiza la matriz de la zona de Carga.
         self.lienzo.create_rectangle(self.X_Destino, self.Y_Destino, self.X_Destino + 40, self.Y_Destino + 40, width=3, fill=color)
 
         self.grua = self.lienzo.create_oval(self.X_Actual, self.Y_Actual, self.X_Actual + 40, self.Y_Actual + 40, width=1, fill="orange")
 
         return
 
-    # Mover la grúa.
-    def mover_Grua(self):
-        #listo = 0
-
-        # Movimiento hacia la derecha.
+    # Mover la grúa horizontalmente.
+    def mover_Grua_X(self):
+        # Movimiento horizontal.
         self.movimiento_X = self.X_Destino - self.X_Actual
-
-        # Movimiento vertical.
-        self.movimiento_Y = self.Y_Destino - self.Y_Actual
 
         # Si ya está en la misma posición en X que el destino.
         if (self.X_Actual == self.X_Destino):
             self.movimiento_X = 0
+
+        self.lienzo.move(self.grua, self.movimiento_X, 0)
+
+        self.X_Actual = self.X_Destino
+        return
+    
+    # Mover la grúa verticalmente.
+    def mover_Grua_Y(self):
+        # Movimiento vertical.
+        self.movimiento_Y = self.Y_Destino - self.Y_Actual
         
         # Si ya está en la misma posición en Y que el destino.
         if (self.Y_Actual == self.Y_Destino):
             self.movimiento_Y = 0
 
-        self.lienzo.move(self.grua, self.movimiento_X, self.movimiento_Y)
-        #listo = 1
+        self.lienzo.move(self.grua, 0, self.movimiento_Y)
 
-        self.X_Actual = self.X_Destino
-        self.Y_Actual = self.Y_Destino
-        
+        self.Y_Actual = self.Y_Destino        
         return
 
     """
